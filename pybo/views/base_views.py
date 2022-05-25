ec4ce8b1e410caf4b20f469bcbc84e65f78eb113
박응용 <pahkey@gmail.com>
2022-05-25 11:23:34 +0900

    정렬 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -11,11 +11,22 @@ def index(request):
     pybo 목록 출력
     """""
     # 입력 인자
-    page = request.GET.get('page', '1')  # 페이지
-    kw = request.GET.get('kw', '')       # 검색어
+    page = request.GET.get('page', '1')     # 페이지
+    kw = request.GET.get('kw', '')          # 검색어
+    so = request.GET.get('so', 'recent')    # 정렬 기준
+
+    # 정렬
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(
+            num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
 
     # 조회
-    question_list = Question.objects.order_by('-create_date')
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |                  # 제목 검색
