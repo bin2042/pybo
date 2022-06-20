311aadae55f9631d6e23bd99aa0a2037f801c0c4
bin2042 <81675855+bin2042@users.noreply.github.com>
2022-06-20 21:07:55 +0900

    카테고리 모델 추가

--- pybo/models.py
+++ pybo/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    has_answer = models.BooleanField(default=True)  # 답변가능 여부
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('pybo:index', args=[self.name])
+
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
